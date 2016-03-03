ff1e5e08a86b7943b9cae4a472cbec3b73972917
Sam Agnew <sagnew@eden.rutgers.edu>
2016-03-03 16:11:12 -0500

    Implement basic getStats call for FireFox

--- conversationStats.js
+++ conversationStats.js
@@ -3,7 +3,45 @@ var accessManager = Twilio.AccessManager(accessToken);
 var client = Twilio.Conversations.Client(accessManager);
 
 function handleConversationStats(conversation) {
-  console.log('Finish this next');
+  var dialog = conversation._dialogs.values().next().value;
+  var peerConnection = dialog.session.mediaHandler.peerConnection;
+  var selector = null;
+  var packets = 0;
+
+  setInterval(function() {
+    Works in FireFox
+    peerConnection.getStats(selector, function(report) {
+      for(var i in report) {
+        var currentReport = report[i];
+        if(currentReport.type === 'outboundrtp') {
+          console.log(currentReport);
+          packets += currentReport.packetsSent;
+        }
+      }
+      console.log('Number of packets sent so far: ' + packets);
+    }, function(error) {
+      console.log('Oh no I messed up: ' + error);
+    });
+
+    // Doesn't work in Chrome yet
+    // peerConnection.getStats(function(report) {
+    //   console.log(report);
+    //   for(var i in report) {
+    //     var currentReport = report[i];
+    //     if(currentReport.type === 'outboundrtp') {
+    //       console.log(currentReport);
+    //       packets += currentReport.packetsSent;
+    //     }
+    //   }
+    //   console.log('Number of packets sent so far: ' + packets);
+    // }, selector, function(error) {
+    //   console.log('Oh no I messed up: ' + error);
+    // });
+  }, 5000);
+}
+
+function standardizeReport(report) {
+  // Implement this next.
 }
 
 function onInviteAccepted(conversation) {
@@ -24,3 +62,4 @@ client.listen().then(function () {
 
 console.log('Hello Keeyon, s_haha_n, Niko, Gutomaia jer_dude, NichtBela and sir_majestic');
 console.log('Also hello to Devin, Brent, Manny and Mheetu');
+console.log('zykO rules and Trohnics is my pizza brother');
